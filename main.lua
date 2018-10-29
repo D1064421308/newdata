711b63cc2fb25aac4cd2ee8dab423358e7ba5394
D1064421308 <apainiorang@hotmail.com>
2018-10-29 19:11:54 +0800

    Update main.lua
    Co-Authored-By: D1064421308 <d1064421308@users.noreply.github.com>

--- main.lua
+++ main.lua
@@ -8,12 +8,6 @@ local world = display.newImageRect( "world.png", 250, 250 )
 world.x = display.contentCenterX
 world.y = display.contentCenterY - 30
 
-local msgText = display.newText( "Hello, world!", world.x, world.y+160, native.systemFont, 32 )
-msgText:setFillColor( 0.2, 0.6, 0.8 )
-
-local msgText = display.newText( "Hello, world!", world.x, world.y+200, native.systemFont, 32 )
-msgText:setFillColor( 0.2, 0.6, 0.8 )
-
-local msgText = display.newText( "Hello, world!", world.x, world.y+240, native.systemFont, 32 )
+local msgText = display.newText( "D1064421308 Hello, world!", world.x, world.y+160, native.systemFont, 32 )
 msgText:setFillColor( 0.2, 0.6, 0.8 )
 
